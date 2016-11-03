5e4a2aeea71c4e83754c91aa2a967f0e3a3482e3
Dan Esparza <esparza.dan@gmail.com>
2016-11-03 15:09:22 -0400

    Added starwars schema

--- main.go
+++ main.go
@@ -1,42 +1,17 @@
 package main
 
 import (
-	"math/rand"
 	"net/http"
 
-	"github.com/graphql-go/graphql"
 	gqlhandler "github.com/graphql-go/handler"
 )
 
 func main() {
 
-	//	The root query and query resolvers
-	rootQuery := graphql.NewObject(graphql.ObjectConfig{
-		Name: "Query",
-		Fields: graphql.Fields{
-			"latestPost": &graphql.Field{
-				Type:        graphql.String,
-				Description: "The latest post",
-				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					return "Hello World!", nil
-				}},
-			"someRandomInt": &graphql.Field{
-				Type:        graphql.Int,
-				Description: "A random int.  No, really",
-				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					return rand.Intn(100), nil
-				}},
-		},
-	})
-
-	//	Setup the schema based on the query
-	schemaConfig := graphql.SchemaConfig{Query: rootQuery}
-	schema, _ := graphql.NewSchema(schemaConfig)
-
 	// create a graphql-go HTTP handler with our previously defined schema
 	// and also set it to return pretty JSON output
 	h := gqlhandler.New(&gqlhandler.Config{
-		Schema: &schema,
+		Schema: &StarWarsSchema,
 		Pretty: true,
 	})
 
